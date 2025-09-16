e39d8dfb1cd0cb85552f73010dceb75f60ec3248
elaizabautista <2023-2-02500@lpunetwork.edu.ph>
2025-09-16 15:40:04 +0800

    new

--- LESSON5/Activity 5.cs
+++ LESSON5/Activity 5.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace LESSON5
 {
@@ -38,3 +39,30 @@ namespace LESSON5
         }
     }
 }
+/* textbox1 // First Name
+textbox2 // Middle Name
+textbox3 // Surname
+textbox4 // Civil Status
+textbox5 // Qualified Dependent Status
+textbox6 // Pay Date
+textbox7 // Employee Status
+textbox8 // Designation
+textbox9 // Employee Number
+textbox10 // Department
+textbox11 // Rate Per Hour in Basic Income
+textbox12 // Number of Hours / Cut-Off in Basic Income
+textbox13 // Income / Cut-Off in Basic Income
+textbox14 // SSS Contribution
+textbox15 // Philhealth Contribution
+textbox16 // Pag-Ibig Contribution
+textbox17 // Income Tax Contribution
+textbox18 // Rate Per Hour in Honorarium Income
+textbox19 // Number of Hours / Cut-Off in Honorarium Income
+textbox20 // Income / Cut-Off in Honorarium Income
+textbox21 // Rate Per Hour in 
+textbox22 // Number of Hours / Cut-Off
+textbox23 // Income / Cut-Off
+textbox24 //
+textbox25 //
+textbox26 //
+textbox27 // /*
