70045ca0d37f5a02591305f73b4a64e3f5f70ab7
mnebarrita <barritanaomi@gmail.com>
2025-12-15 10:44:55 +0800

    sqlpos1

--- mainsystem/Program.cs
+++ mainsystem/Program.cs
@@ -18,7 +18,7 @@ namespace mainsystem
             //Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new sales_reports());
+            Application.Run(new SQLPOS1Class());
         }
     }
 }
